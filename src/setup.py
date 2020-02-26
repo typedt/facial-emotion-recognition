cc2b8d530a771fa52b99a0c86e224578249790a1
typedt <donnatxj@gmail.com>
2020-02-25 23:18:57 -0800

    Add setup script for python packaging

---
+++ src/setup.py
@@ -0,0 +1,13 @@
+# -*- coding: UTF-8 -*-
+
+from setuptools import setup
+
+
+setup(name='faceemo',
+      version='0.1',
+      description='A simple face emotion detection library',
+      url='https://github.com/typedt/facial-emotion-recognition',
+      author='Donna Tang',
+      license='MIT',
+      packages=['faceemo'],
+      zip_safe=False)
