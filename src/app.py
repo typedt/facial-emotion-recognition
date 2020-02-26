cc56cc283e038274bd4e2cc02f6d552c847092d7
typedt <donnatxj@gmail.com>
2020-02-25 18:09:24 -0800

    Rename classes in utils, add landmark affine transformation, update notebook with affine transformation validation steps

--- src/app.py
+++ src/app.py
@@ -3,16 +3,16 @@
 import cv2
 import numpy as np
 import dlib
-import curses
 import os
-from faceemo.utils import FeatureExtractor, FaceAligner
+from faceemo.utils import *
 
 
 if __name__ == '__main__':
     # init the FeatureExtractor object
     predictor_file = os.path.abspath("data/support/shape_predictor_68_face_landmarks.dat")
+    predictor = dlib.shape_predictor(predictor_file)
+    detector = FaceDetector(predictor)
     aligner = FaceAligner()
-    extractor = FeatureExtractor(predictor_file, aligner)
 
     # open webcam
     cap = cv2.VideoCapture(0)
@@ -24,20 +24,19 @@ if __name__ == '__main__':
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         # mark landmarks
-        landmarks = extractor.get_landmarks(gray)
-        extractor.circle_landmarks(frame, landmarks)
-        cv2.imshow('Image Captured with Landmarks', frame)
-
-        # show aligned face with landmarks
+        landmarks = detector.get_landmarks(gray)
         if landmarks is not None:
-            extractor.circle_landmarks(gray, landmarks)
-            aligned = extractor.get_aligned_face(gray, landmarks)
+            # circle_landmarks(frame, landmarks)
+            # cv2.imshow('Image Captured with Landmarks', frame)
+            aligned = aligner.align_image(gray, landmarks)
+            landmarks_af = aligner.align_landmarks(landmarks, round=True)
+            circle_landmarks(aligned, landmarks_af)
             cv2.imshow('Aligned face', aligned)
 
         # exit when user presses `q`
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-# release web cam handle and clean up
-cap.release()
-cv2.destroyAllWindows()
+    # release web cam handle and clean up
+    cap.release()
+    cv2.destroyAllWindows()
--- src/faceemo/utils.py
+++ src/faceemo/utils.py
@@ -6,33 +6,36 @@ import numpy as np
 import dlib
 
 
-
 FACIAL_LANDMARKS_68_IDXS = OrderedDict([
-	("mouth", (48, 68)),
-	("inner_mouth", (60, 68)),
-	("right_eyebrow", (17, 22)),
-	("left_eyebrow", (22, 27)),
-	("right_eye", (36, 42)),
-	("left_eye", (42, 48)),
-	("nose", (27, 36)),
-	("jaw", (0, 17))
+    ("mouth", (48, 68)),
+    ("inner_mouth", (60, 68)),
+    ("right_eyebrow", (17, 22)),
+    ("left_eyebrow", (22, 27)),
+    ("right_eye", (36, 42)),
+    ("left_eye", (42, 48)),
+    ("nose", (27, 36)),
+    ("jaw", (0, 17))
 ])
 
 
-class FeatureExtractor:
-    def __init__(self, predictor_file, aligner):
+def circle_landmarks(img, landmarks, color=(0,0,255), thickness=2):
+    if landmarks is None:
+        return
+    if isinstance(landmarks, np.ndarray):
+        for (x, y) in landmarks:
+            cv2.circle(img, (x, y), 1, color, thickness=thickness)
+
+
+class FaceDetector:
+
+    def __init__(self, predictor):
         '''
         Initialize a FeatureExtractor.
 
         Args:
-        predictor_file: a front face shape predictor file
-        aligner: a FaceAligner object that normalizes a face shape
+        predictor: a dlib shape predictor
         '''
-        self.predictor = self._get_predictor(predictor_file)
-        self.aligner = aligner
-
-    def _get_predictor(self, predictor_file):
-        return dlib.shape_predictor(predictor_file)
+        self.predictor = predictor
 
     def _get_biggest_face(self, faces):
         maxarea = 0
@@ -65,30 +68,30 @@ class FeatureExtractor:
         landmarks = [(shape.part(i).x, shape.part(i).y) for i in range(68)]
         return np.asarray(landmarks)
 
-    def circle_landmarks(self, img, landmarks):
-        if landmarks is None:
-            return
-        if isinstance(landmarks, np.ndarray):
-            for (x, y) in landmarks:
-                cv2.circle(img, (x, y), 1, (0,0,255), thickness=5)
-        else:
-            raise ValueError("landmarks should be ndarray")
-
-    def get_aligned_face(self, img, landmarks):
-        return self.aligner.align(img, landmarks)
-
-    def get_features(self, landmarks):
-        pass
-
 
 class FaceAligner:
-    def __init__(self, desiredLeftEye=(0.35, 0.35),
-                 desiredFaceWidth=256, desiredFaceHeight=256):
-        self.desiredLeftEye = desiredLeftEye
-        self.desiredFaceWidth = desiredFaceWidth
-        self.desiredFaceHeight = desiredFaceHeight
 
-    def align(self, img, landmarks):
+    def __init__(self, desiredLeftEye=(0.35, 0.35),
+                 desiredWidth=256, desiredHeight=256):
+        '''
+        Initialize a FaceAligner.
+
+        Args:
+        desiredLeftEye: the desired location of the center of the left eye,
+                        compared with the entire output image of the aligner.
+                        The biggner the numbers are, the more zoomed-out the
+                        face will be.
+        desiredWidth: width of the output image.
+        desiredHeight: height of the output image.
+        '''
+        self.desiredLeftEye = desiredLeftEye
+        self.desiredWidth = desiredWidth
+        self.desiredHeight = desiredHeight
+
+    def _getAffineMatrix(self, landmarks):
+        '''
+        Get affine matrix from landmark coordinates.
+        '''
         # get left and right eye coordinates
         (lStart, lEnd) = FACIAL_LANDMARKS_68_IDXS["left_eye"]
         (rStart, rEnd) = FACIAL_LANDMARKS_68_IDXS["right_eye"]
@@ -108,7 +111,7 @@ class FaceAligner:
         # desired x-coordinate of the left eye
         dist = np.linalg.norm(leftEyeCenter - rightEyeCenter)
         desiredDist = 1.0 - 2 * self.desiredLeftEye[0]
-        desiredDist *= self.desiredFaceWidth
+        desiredDist *= self.desiredWidth
         scale = desiredDist / dist
 
         # center of two eyes
@@ -119,14 +122,41 @@ class FaceAligner:
         M = cv2.getRotationMatrix2D(eyesCenter, angle, scale)
 
         # update the translation component of the matrix
-        # TODO: figure this out
-        tX = self.desiredFaceWidth * 0.5
-        tY = self.desiredFaceHeight * self.desiredLeftEye[1]
+        # recenter the output image to be center of eyes
+        tX = self.desiredWidth * 0.5
+        tY = self.desiredHeight * self.desiredLeftEye[1]
         M[0, 2] += (tX - eyesCenter[0])
         M[1, 2] += (tY - eyesCenter[1])
 
+        return M
+
+    def align_image(self, img, landmarks):
+        '''
+        Do affine transformation of the original image. The output image is
+        scaled to certain size, and rotated and translated, so that the two
+        eyes of the face are horizontally aligned.
+
+        Returns:
+        The output image.
+        '''
+        M = self._getAffineMatrix(landmarks)
+
         # apply the affine transformation
-        (w, h) = (self.desiredFaceWidth, self.desiredFaceHeight)
+        (w, h) = (self.desiredWidth, self.desiredHeight)
         output = cv2.warpAffine(img, M, (w, h),flags=cv2.INTER_CUBIC)
 
         return output
+
+    def align_landmarks(self, landmarks, round=False):
+        '''
+        Do affine transformation of the landmarks.
+        https://docs.opencv.org/2.4/doc/tutorials/imgproc/imgtrans/warp_affine/warp_affine.html
+        '''
+        M = self._getAffineMatrix(landmarks)
+        A = M[:, :2]; B = M[:, 2]
+        landmarks_ltfm = np.dot(A, landmarks.T)
+        landmarks_af = np.add(landmarks_ltfm.T, B)
+        if round:
+            return np.rint(landmarks_af).astype(int)
+        else:
+            return landmarks_af
